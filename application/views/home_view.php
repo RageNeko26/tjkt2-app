6e026f7157b2fab7d8417092612d9433adba766d
RageNeko26 <94698622+RageNeko26@users.noreply.github.com>
2022-10-10 22:01:58 +0700

    Add files via upload
    Update section fitur

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -4,7 +4,7 @@ require_once 'components/header.php';
 
 <nav class="navbar navbar-expand-lg bg-primary ">
   <div class="container">
-    <a class="navbar-brand text-white" href="#">Mapel Pilihan</a>
+    <a class="navbar-brand text-white" href="#"><img src="<?php echo base_url("assets/logo.png") ?>" width="80" alt="logo"></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -26,9 +26,72 @@ require_once 'components/header.php';
 </nav>
 
 <section id="home">
-    <div class="container">
-       
+    <div class="container mt-3">
+       <div class="row">
+          <div class="col-md-8 ">
+              <h2 style="color:#E6C86F;" class="inter-font">Aplikasi TJKT 2</h2>
+              <p class="fs-2">Sebuah Platform Untuk Kepentingan Pembelajaran dan Sebagai Project Sampingan</p>
+          </div>
+          <div class="col-md-4">
+            <img src="<?php echo base_url("assets/logo.png") ?>" width="300" alt="logo_kelas">
+          </div>
+       </div>
     </div>
+</section>
+
+<section id="info">
+  <div class="container" style="margin-top:180px">
+    <div class="row">
+      <div class="col mb-3">
+        <h2 class="text-center">Fitur</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4 mb-5 ">
+            <div class="card p-4 shadow " style="width: 18rem;">
+              <span>
+              <img src="<?php echo base_url("assets/fitur1.svg") ?>" width="200" alt="fitur">
+              </span>
+              <div class="card-body">
+                <h5 class="card-title">Submission Tugas</h5>
+                <p class="card-text gray">Sebuah fitur yang dikhususkan untuk mengumpulkan tugas berbasis online</p>
+              </div>
+      </div>
+      </div>
+      <div class="col-md-4 mb-5 ">
+            <div class="card p-4 shadow " style="width: 18rem;">
+              <span>
+              <img src="<?php echo base_url("assets/fitur2.svg") ?>" width="150" alt="fitur">
+              </span>
+              <div class="card-body">
+                <h5 class="card-title">Integrasi API</h5>
+                <p class="card-text gray">Ingin terhubung ke platform kami? Anda bisa dengan mudah mengintegrasikan aplikasi anda ke platform kami</p>
+              </div>
+      </div>
+      </div>
+      <div class="col-md-4 mb-5 ">
+            <div class="card p-4  shadow " style="width: 18rem;">
+              <span>
+              <img src="<?php echo base_url("assets/fitur3.svg") ?>" width="250" height="200" alt="fitur">
+              </span>
+              <div class="card-body">
+                <h5 class="card-title">CMS</h5>
+                <p class="card-text gray">Fitur di mana pengguna bisa mengelola konten yang ada di dalam website</p>
+              </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="tugas">
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col">
+        <h2 class="text-center">Tugas</h2>
+      </div>
+    </div>
+  </div>
 </section>
 
 
